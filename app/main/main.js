59fa15e83574ca679a3613dcdba99b10b9e64d1a
Oleg Mykhailov <o.mykhailov@do-it.co>
2019-09-06 15:04:10 +0300

    Basic skeleton added.

---
+++ app/main/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp.main', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/main', {
+    templateUrl: 'main/main.html',
+    controller: 'MainCtrl'
+  });
+}])
+
+.controller('MainCtrl', ['$scope', function($scope) {
+  var $ = jQuery;
+
+  console.log($('.container'));
+}]);
